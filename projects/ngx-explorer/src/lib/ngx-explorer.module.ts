b2e99aaa470158831fdc940932fc82f4dabedeec
artemnih <23387542+artemnih@users.noreply.github.com>
2021-10-01 13:48:06 -0400

    chore: removed example component

--- projects/ngx-explorer/src/lib/ngx-explorer.module.ts
+++ projects/ngx-explorer/src/lib/ngx-explorer.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { NgxExplorerComponent } from './ngx-explorer.component';
-import { ExampleComponent } from './example/example.component';
 import { IconsComponent } from './icons/icons.component';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     NgxExplorerComponent,
-    ExampleComponent,
     IconsComponent
   ],
   imports: [
