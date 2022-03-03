c1dcb6d0ce6dfa32c3f9448c7cc9d9f0f1393355
artemnih <23387542+artemnih@users.noreply.github.com>
2022-03-03 13:54:41 -0500

    fix: add sub to explorer

--- projects/ngx-explorer/src/lib/components/explorer/explorer.component.ts
+++ projects/ngx-explorer/src/lib/components/explorer/explorer.component.ts
@@ -1,5 +1,5 @@
-import { Component, Inject, ViewEncapsulation } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Component, Inject, OnDestroy, ViewEncapsulation } from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { AvialableView } from '../../common/types';
 import { CURRENT_VIEW } from '../../injection-tokens/current-view.token';
 
@@ -9,15 +9,20 @@ import { CURRENT_VIEW } from '../../injection-tokens/current-view.token';
     styleUrls: ['./explorer.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
-export class ExplorerComponent {
+export class ExplorerComponent implements OnDestroy {
 
     public avialableView = AvialableView;
     public view: string;
+    private sub = new Subscription();
 
     constructor(@Inject(CURRENT_VIEW) private currentView: BehaviorSubject<AvialableView>) {
-        this.currentView.subscribe(view => {
+        this.sub.add(this.currentView.subscribe(view => {
             this.view = view;
-        })
+        }));
     }
 
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+    
 }
