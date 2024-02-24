cdee72cdae6adcb83ca15bc3745d920cd180097c
artemnih <23387542+artemnih@users.noreply.github.com>
2024-02-24 12:51:38 -0500

    use async pipe instead of subscription

--- projects/ngx-explorer/src/lib/components/explorer/explorer.component.ts
+++ projects/ngx-explorer/src/lib/components/explorer/explorer.component.ts
@@ -1,5 +1,5 @@
-import { Component, Inject, OnDestroy, ViewEncapsulation } from '@angular/core';
-import { BehaviorSubject, Subscription } from 'rxjs';
+import { ChangeDetectionStrategy, Component, Inject, ViewEncapsulation } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { AvialableView } from '../../shared/types';
 import { CURRENT_VIEW } from '../../shared/providers';
 import { MenuBarComponent } from '../menu-bar/menu-bar.component';
@@ -7,6 +7,7 @@ import { TreeComponent } from '../tree/tree.component';
 import { SecondMenuBarComponent } from '../second-menu-bar/second-menu-bar.component';
 import { IconsComponent } from '../icons/icons.component';
 import { ListComponent } from '../list/list.component';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
     selector: 'nxe-explorer',
@@ -14,22 +15,12 @@ import { ListComponent } from '../list/list.component';
     styleUrls: ['./explorer.component.scss'],
     encapsulation: ViewEncapsulation.None,
     standalone: true,
-    imports: [MenuBarComponent, TreeComponent, SecondMenuBarComponent, IconsComponent, ListComponent]
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [AsyncPipe, MenuBarComponent, TreeComponent, SecondMenuBarComponent, IconsComponent, ListComponent]
 })
-export class ExplorerComponent implements OnDestroy {
-
+export class ExplorerComponent {
     public avialableView = AvialableView;
-    public view?: string;
-    private sub = new Subscription();
+    public view$ = this.currentView.asObservable();
 
-    constructor(@Inject(CURRENT_VIEW) private currentView: BehaviorSubject<AvialableView>) {
-        this.sub.add(this.currentView.subscribe(view => {
-            this.view = view;
-        }));
-    }
-
-    ngOnDestroy() {
-        this.sub.unsubscribe();
-    }
-
+    constructor(@Inject(CURRENT_VIEW) private currentView: BehaviorSubject<AvialableView>) {}
 }
